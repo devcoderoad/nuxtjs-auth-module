b138780ff855ebe0cc139bc4ff4444e5a6a4a78b
Defrian Yarfi <devdef20@gmail.com>
2022-11-08 22:17:10 +0700

    chore: add login page

---
+++ pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>Login</div>
+    <v-form>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="username"
+            :rules="{}"
+            label="Username"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="password"
+            type="password"
+            :rules="{}"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn type="submit" elevation="2">Submit</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthLogin',
+  data() {
+    return { username: '', password: '' }
+  },
+}
+</script>
